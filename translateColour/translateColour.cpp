a9b59a14c627231b6c6130d055cc7a005233c15f
Javier Utrera Cruzado <44610496+Javierutr@users.noreply.github.com>
2018-12-27 21:16:24 +0100

    Add files via upload

--- translateColour/translateColour.cpp
+++ translateColour/translateColour.cpp
@@ -1,50 +1,49 @@
-//Given a RGB colour value (0-255)
+///Given a RGB colour value (0-255)
 //this library will compare with previously defined (in this library) colors.
 //Example: #include "translateColour.h"
 //whatColour(R, G, B, colour), being colour a char.
 
-#include "translateColour.h"
 #include "Arduino.h"
+#include "translateColour.h"
 
-translateColour::translateColour()
-{
-int RED[3]={245,180,190};
-int GREEN[3]={205,230,195};
-int YELLOW[3]={255,250,230};
-int WHITE[3]={255,255,255};
-int BLUE[3]={193,203,234};
-int tol=20;
-}
 
 void translateColour::whatColour(int R, int G, int B, char & color)
 {
-	if (      ((R-tol<RED[0])&&(G-tol<RED[1]) && (B-tol<RED[2]))  &&
-(   (R+tol>RED[0])&&(G+tol>RED[1]) && (B+tol>RED[2])  )      )  
+	if ( ((R-tol<RED[0])&&(G-tol<RED[1]) && (B-tol<RED[2]))  &&
+(   (R+tol>RED[0])&&(G+tol>RED[1]) && (B+tol>RED[2])  )      )
 {
-  Serial.println("   RED");
+  Serial.println('R');
+  color = 'R';
 }
 
 else if (     ((R-tol<GREEN[0])&&(G-tol<GREEN[1]) && (B-tol<GREEN[2]))  &&
-(   (R+tol>GREEN[0])&&(G+tol>GREEN[1]) && (B+tol>GREEN[2])  )      )  
+(   (R+tol>GREEN[0])&&(G+tol>GREEN[1]) && (B+tol>GREEN[2])  )      )
 {
-  Serial.println("   GREEN");
+  Serial.println('G');
+  color = 'G';
 }
 
 else if (     ((R-tol<YELLOW[0])&&(G-tol<YELLOW[1]) && (B-tol<YELLOW[2]))  &&
-(   (R+tol>YELLOW[0])&&(G+tol>YELLOW[1]) && (B+tol>YELLOW[2])  )      )  
+(   (R+tol>YELLOW[0])&&(G+tol>YELLOW[1]) && (B+tol>YELLOW[2])  )      )
 {
-  Serial.println("   YELLOW");
+  Serial.println('Y');
+  color = 'Y';
 }
 
 else if (     ((R-tol<WHITE[0])&&(G-tol<WHITE[1]) && (B-tol<WHITE[2]))  &&
-(   (R+tol>WHITE[0])&&(G+tol>WHITE[1]) && (B+tol>WHITE[2])  )      )  
+(   (R+tol>WHITE[0])&&(G+tol>WHITE[1]) && (B+tol>WHITE[2])  )      )
 {
-  Serial.println("   WHITE");
+  Serial.println('W');
+  color = 'W';
 }
 
 else if (     ((R-tol<BLUE[0])&&(G-tol<BLUE[1]) && (B-tol<BLUE[2]))  &&
-(   (R+tol>BLUE[0])&&(G+tol>BLUE[1]) && (B+tol>BLUE[2])  )      )  
+(   (R+tol>BLUE[0])&&(G+tol>BLUE[1]) && (B+tol>BLUE[2])  )      )
 {
-  Serial.println("   BLUE");
+  Serial.println('B');
+  color = 'B';
+}
+else {
+	color= 'N';
 }
 }
--- translateColour/translateColour.h
+++ translateColour/translateColour.h
@@ -6,13 +6,23 @@
 class translateColour
 {
 	public:
-		translateColour();
+		translateColour() {
+		    tol = 30;
+		    RED[0] =230; RED[1]=160; RED[2]=170;
+            GREEN[0]=205; GREEN[1]=230; GREEN[2]=195;
+            //YELLOW[0]=255; YELLOW[1]=250; YELLOW[2]=230;
+            WHITE[0]=245; WHITE[1]=245;WHITE[2]=245;
+            BLUE[0]=100; BLUE[1]=170; BLUE[2]=200;
+		};
 		void whatColour(int R,int G,int B,char & color);
-		
+
 	private:
-		int _R; int _G; int _B; int WHITE[3]; int YELLOW[3];
-		int RED[3]; int GREEN[3]; int BLUE[3]; int tol;
-	
+	    int RED[3];
+	    int GREEN[3];
+        int YELLOW[3];
+        int WHITE[3];
+        int BLUE[3];
+        int tol;
 };
 
 #endif
